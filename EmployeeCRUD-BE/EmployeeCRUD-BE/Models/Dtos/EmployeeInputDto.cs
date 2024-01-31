0bbf5e8371ad7992cdbf979c68ff2d9f94bb4c4b
karim.mohamed <karim.mohamed@TECH-KARIM2>
2024-01-31 14:45:17 +0200

    .

--- EmployeeCRUD-BE/EmployeeCRUD-BE/Models/Dtos/EmployeeInputDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeCRUD_BE.Models.Dtos
-{
-    public class EmployeeInputDto
-    {
-        public string? Name { get; set; }
-        public string? Age { get; set; }
-        public int isActive { get; set; }
-    }
-}
